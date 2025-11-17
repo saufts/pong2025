7bfd789d3497cfd3b94b04bfd1dd2e1f99069584
lukasz <l.szwaczka90@gmail.com>
2025-11-17 19:28:30 +0100

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#test3 pong
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+screen = Screen()
+screen.bgcolor('black')
+
+paddle = Paddle()
+
+screen.setup(width=800, height=600)
+screen.title('Pong')
+
+
+screen.exitonclick()
