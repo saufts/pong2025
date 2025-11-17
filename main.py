e0e71ae75ab6318b053b5e6fb3dac9bdc6fac56c
lukasz <l.szwaczka90@gmail.com>
2025-11-18 00:26:01 +0100

    detect ball collisions with paddles and bounce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor('black')
@@ -9,6 +11,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+
+ball = Ball()
 
 
 game_is_on = True
@@ -20,7 +24,26 @@ screen.onkey(r_paddle.go_up, 'Up')
 screen.onkey(r_paddle.go_down, 'Down')
 
 while game_is_on:
+
+    ball.move()
     screen.update()
+    time.sleep(0.1)
+
+    #detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_top()
+
+    #detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_paddle()
+
+    #Detect R paddle misses
+    if ball.xcor() > 380 or ball.xcor() < -380:
+        ball.goto(0,0)
+        ball.bounce_paddle()
+
+
+
 
 
 
